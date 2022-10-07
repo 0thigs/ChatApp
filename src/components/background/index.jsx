d2358027d1b83f5fbc132f3fab29e7f684b8b8fc
0thigs <machinimavlogs@gmail.com>
2022-10-07 18:37:50 -0300

    Connecting with Database

--- src/components/background/index.jsx
+++ src/components/background/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './style.css'
 import Card from '../card'
+import { useState, useEffect } from 'react';
+import { supabase } from '../../supabaseClient';
 
 export default function Background() {
+
+    async function Init() {
+        const { data: profile, error } = await supabase
+        .from('profiles')
+        .select('*')
+        console.log(profile)
+    }
+
+   Init()
+
     return (
         <div className='container'>
             <Card />
